dc84df34810ee03b710aafe0ad5d53497014e3ed
Tao Wang <hellotaotao@gmail.com>
2021-05-18 02:50:26 +0930

    add a DateTimeParseController to enable checking local timezone of the running instance.

---
+++ DateCalculate/Controllers/DateTimeParseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DateCalculate.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DateCalculate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateTimeParseController : ControllerBase
+    {
+        // GET: api/<DaysController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] DateTimeParseRequest dtr)
+        {
+            var dt = DateTime.Parse(dtr.Dt);
+            return Ok(dt.ToString());
+        }
+    }
+
+    public class DateTimeParseRequest
+    {
+        public string Dt { get; set; }
+    }
+}
