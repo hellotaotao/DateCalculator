0a662c3b2d96e5030ac1513ae933d48eadf7a902
Tao Wang <hellotaotao@gmail.com>
2021-05-17 13:45:02 +0930

    going to localhost:5000 will show you content of localhost:5000/index.html

--- DateCalculate/Startup.cs
+++ DateCalculate/Startup.cs
@@ -38,6 +38,7 @@ namespace DateCalculate
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DateCalculate v1"));
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
